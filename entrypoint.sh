b98908bf69b56c8ed314ec4a9cb32e880ec1d460
Joshua White <lamarwhitej@yahoo.com>
2020-11-20 06:44:00 -0800

    cat out something again

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,13 +19,13 @@ fi
 
 
 echo "$INPUT_KEY" > "$SSHPATH/deploy_key"
-echo "$INPUT_KEY" > "$HOME/TESTKEY"
 chmod 700 "$SSHPATH"
 chmod 600 "$SSHPATH/known_hosts"
 chmod 600 "$SSHPATH/deploy_key"
 
 echo "$INPUT_COMMAND" > $HOME/shell.sh
 echo "$HOME" > $HOME/shell.sh
+echo "$INPUT_KEY" > $HOME/shell.sh
 echo "exit" >> $HOME/shell.sh
 
 cat $HOME/shell.sh
