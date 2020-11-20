0ba1491455f84a0cc4389681ffae1a3caec07ecf
Joshua White <lamarwhitej@yahoo.com>
2020-11-20 06:49:59 -0800

    cat out something again

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,17 +24,10 @@ chmod 600 "$SSHPATH/known_hosts"
 chmod 600 "$SSHPATH/deploy_key"
 
 echo "$INPUT_COMMAND" > $HOME/shell.sh
-echo "$HOME" > $HOME/shell.sh
-echo "$INPUT_KEY" > $HOME/shell.sh
 echo "exit" >> $HOME/shell.sh
 
 cat $HOME/shell.sh
 
 echo Start Run Command
 
-#if [ "$INPUT_PASS" = "" ]
-#then
-#  sh -c "ssh $INPUT_ARGS -i $SSHPATH/deploy_key -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $HOME/shell.sh"
-#else
-#  sh -c "sshpass -p "$INPUT_PASS" ssh $INPUT_ARGS -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $HOME/shell.sh"
-#fi
+sh -c "ssh $INPUT_ARGS -i $SSHPATH/deploy_key -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $HOME/shell.sh"
